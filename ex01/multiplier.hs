bb890db8befb857d7c41444a072395deb2ec7324
Ayg0 <taha.eddafili@gmail.com>
2024-08-15 17:39:32 +0100

    added decrement

--- ex01/multiplier.hs
+++ ex01/multiplier.hs
@@ -1,14 +1,19 @@
 import Adder (adder) 
 import Data.Word (Word32)
+import Data.Bits (complement)
 
 main:: IO()
 multiplier :: Word32 -> Word32 -> Word32
+decrement:: Word32 -> Word32
+
+----------------------------------------------------------------
+decrement nbr = adder nbr ((complement 1) + 1)
 
 ----------------------------------------------------------------
 multiplier nbr1 nbr2 = aux nbr1 nbr2 0
  where
   aux nbr1 nbr2 res
    | nbr2 == 0 = res
-   | otherwise = aux nbr1 (nbr2 - 1) (adder res nbr1)
+   | otherwise = aux nbr1 (decrement nbr2) (adder res nbr1)
 
-main = print(multiplier 10 4)
+main = print(multiplier 55 2)
